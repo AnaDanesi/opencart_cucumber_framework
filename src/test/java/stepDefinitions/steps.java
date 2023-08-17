85a718b7805271cdedc416836d830fafce86ba35
Anabella Danesi <anetadanesi@gmail.com>
2023-08-17 11:41:39 +0300

    creating testData directory with excel sheet

--- src/test/java/stepDefinitions/steps.java
+++ src/test/java/stepDefinitions/steps.java
@@ -18,6 +18,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import pageObjects.HomePage;
 import pageObjects.LoginPage;
 import pageObjects.MyAccountPage;
+import utilities.DataReader;
 
 import java.time.Duration;
 import java.util.HashMap;
@@ -126,5 +127,66 @@ public class steps {
         }
 
     }
+
+    //*******   Data Driven test method    **************
+    @Then("check User navigates to MyAccount Page by passing Email and Password with excel row {string}")
+    public void check_user_navigates_to_my_account_page_by_passing_email_and_password_with_excel_data(String rows)
+    {
+        datamap= DataReader.data(System.getProperty("user.dir")+"\\testData\\Opencart_LoginData.xlsx", "Sheet1");
+
+        int index=Integer.parseInt(rows)-1;
+        String email= datamap.get(index).get("username");
+        String pwd= datamap.get(index).get("password");
+        String exp_res= datamap.get(index).get("res");
+
+        lp=new LoginPage(driver);
+        lp.setEmail(email);
+        lp.setPassword(pwd);
+
+        lp.clickLogin();
+        try
+        {
+            boolean targetpage=macc.isMyAccountPageExists();
+
+            if(exp_res.equals("Valid"))
+            {
+                if(targetpage==true)
+                {
+                    MyAccountPage myaccpage=new MyAccountPage(driver);
+                    myaccpage.clickLogout();
+                    Assert.assertTrue(true);
+                }
+                else
+                {
+                    Assert.assertTrue(false);
+                }
+            }
+
+            if(exp_res.equals("Invalid"))
+            {
+                if(targetpage==true)
+                {
+                    macc.clickLogout();
+                    Assert.assertTrue(false);
+                }
+                else
+                {
+                    Assert.assertTrue(true);
+                }
+            }
+
+
+        }
+        catch(Exception e)
+        {
+
+            Assert.assertTrue(false);
+        }
+        driver.close();
+    }
+
+    //*******   Account Registration Methods    **************
+
+
 
 }
